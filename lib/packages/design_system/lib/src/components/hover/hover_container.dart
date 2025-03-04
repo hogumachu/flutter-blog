01ac6d9e59d1a96e4a95e05c4a185b33cc3377cf
sungjun-hong-ikoob <74225754+hogumachu@users.noreply.github.com>
2025-03-04 10:22:17 +0900

    add hover container

--- lib/packages/design_system/lib/src/components/hover/hover_container.dart
+++ lib/packages/design_system/lib/src/components/hover/hover_container.dart
@@ -15,6 +15,32 @@ class HoverContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final theme = Theme.of(context);
+    final textTheme = theme.textTheme;
+    final iconTheme = theme.iconTheme;
+    final hoverTheme = theme.copyWith(
+      textTheme: textTheme.copyWith(
+        bodyMedium: textTheme.bodyMedium?.copyWith(
+          color: style.foregroundColorOnHover ?? theme.colorScheme.onSurface,
+        ),
+      ),
+      iconTheme: iconTheme.copyWith(
+        color: style.foregroundColorOnHover ?? theme.colorScheme.onSurface,
+      ),
+    );
+    return Container(
+      margin: style.contentMargin,
+      decoration: BoxDecoration(
+        border: style.border,
+        color: applyStyle
+            ? style.hoverColor ?? Theme.of(context).colorScheme.secondary
+            : style.backgroundColor,
+        borderRadius: style.borderRadius,
+      ),
+      child: Theme(
+        data: applyStyle ? hoverTheme : Theme.of(context),
+        child: child,
+      ),
+    );
   }
 }
