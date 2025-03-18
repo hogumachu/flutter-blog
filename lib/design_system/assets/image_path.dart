7d5ce39811a79d4bdcb9893dc034515b06f1190f
sungjun-hong-ikoob <74225754+hogumachu@users.noreply.github.com>
2025-03-18 11:20:06 +0900

    add emoji assets

---
+++ lib/design_system/assets/image_path.dart
@@ -0,0 +1,10 @@
+class ImagePath {
+  const ImagePath._();
+
+  static const basePath = 'assets/images';
+
+  // Emojis
+  static const grinningSquintingFace = '$basePath/grinning_squinting_face.svg';
+  static const mobilePhone = '$basePath/mobile_phone.svg';
+  static const wavingHand = '$basePath/waving_hand.svg';
+}
--- lib/design_system/design_system.dart
+++ lib/design_system/design_system.dart
@@ -2,3 +2,4 @@ library;
 
 export 'components/button/blog_text_button.dart';
 export 'styled_context.dart';
+export 'assets/image_path.dart';
